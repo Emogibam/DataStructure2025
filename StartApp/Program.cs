2684064f1592ea61d1a57d98f7082e96c071b557
Emmanuel Ogidan <emmanuelogidan@bepeerless.co>
2025-01-03 13:30:37 +0100

    Add project files.

---
+++ StartApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more
+// 
+
+using Arrays;
+
+CustomArray<int> array = new CustomArray<int>(2);
+
+array.Add(1);
+array.Add(2);
+
+foreach(var item in array)
+{
+    Console.WriteLine(item);
+}
